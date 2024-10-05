a6e3d6c92a5e3d8f09cc0d4f9ad715007e0277d6
Anna Komrakova <komrakova.anna@gmail.com>
2024-10-05 19:55:23 +0100

    fix

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,9 +1,10 @@
 import { RotatingLines } from "react-loader-spinner";
+import css from "./Loader.module.css";
 
 const Loader = () => {
   return (
-    <div className="loader">
-      <RotatingLines color="#b43583" height={64} width={64} />
+    <div className={css.loader}>
+      <RotatingLines color="#60a3ea" height={64} width={64} />
     </div>
   );
 };
